bf73eb62de4720f21d5b994c7b5941f9cb9552bd
SurjeshYadavMasai <surjeshom@gmail.com>
2024-02-07 10:50:23 +0530

    changes

--- app/components/SuperchargeSections/Supercharge.jsx
+++ app/components/SuperchargeSections/Supercharge.jsx
@@ -19,6 +19,7 @@ const Supercharge = () => {
       <div>
         <div className="grid sm:grid-cols-1 md:grid-cols-1 gap-4">
           <div
+            data-aos="fade-up"
             data-w-id="b3fd95f7-10b2-f103-e2b0-e28f97e2adb1"
             className="df-item p-6 py-8 md:flex  justify-around background_box"
           >
@@ -30,18 +31,17 @@ const Supercharge = () => {
                 className="icon-medium"
               />
               <h3 className="text-2xl  mt-4">Doller House 1 Plan</h3>
-
-              <p className=" text-white mt-3">
+              <p className=" max-w-[350px] text-white mt-3">
                 The best & advanced networking platform deployed on Binance
                 smart chain network, with superfast transactions & speed. The
                 house of investment is coming for you all..
               </p>
             </div>
-            <div className="max-w-[260px] mt-4 md:mt-0 mx-auto md:mx-0">
+            <div className="max-w-[425px] mt-4 md:mt-0 mx-auto md:mx-0">
               <img src="/featuresBox.svg" />
             </div>
           </div>
-          <div data-w-id="b3fd95f7-10b2-f103-e2b0-e28f97e2adb1 flex justify-between"
+          <div  data-aos="fade-up" data-w-id="b3fd95f7-10b2-f103-e2b0-e28f97e2adb1 flex justify-between"
             className="df-item p-6 py-8 md:flex justify-around background_box"
           >
             <div className="md:w-1/2 mt-3">
@@ -53,17 +53,18 @@ const Supercharge = () => {
               />
               <h3 className="text-2xl  mt-4">Doller House 1 Plan</h3>
 
-              <p className=" mt-3 text-white">
+              <p className="max-w-[350px] mt-3 text-white">
                 The best & advanced networking platform deployed on Binance
                 smart chain network, with superfast transactions & speed. The
                 house of investment is coming for you all..
               </p>
             </div>
-            <div className="max-w-[260px] mt-4 md:mt-0 mx-auto md:mx-0">
-              <img src="/featuresBox.svg" />
+            <div className="max-w-[500px] mt-4 md:mt-0 mx-auto md:mx-0">
+              <img src="/featuresBox2.svg" />
             </div>
           </div>
           <div
+           data-aos="fade-up"
             data-w-id="b3fd95f7-10b2-f103-e2b0-e28f97e2adb1 flex justify-between"
             className="df-item  p-6 py-8 md:flex justify-around background_box"
           >
@@ -75,14 +76,37 @@ const Supercharge = () => {
                 className="icon-medium"
               />
               <h3 className="text-2xl mt-4">Doller House 1 Plan</h3>
-              <p className="text-white mt-3">
+              <p className="text-white mt-3 max-w-[350px]">
                 The best & advanced networking platform deployed on Binance
                 smart chain network, with superfast transactions & speed. The
                 house of investment is coming for you all..
               </p>
             </div>
-            <div className="max-w-[260px] mt-4 md:mt-0 mx-auto md:mx-0">
-              <img src="/featuresBox.svg" />
+            <div className="max-w-[425px] mt-4 md:mt-0 mx-auto md:mx-0">
+              <img src="/featuresBox3.svg" />
+            </div>
+          </div>
+          <div
+           data-aos="fade-up"
+            data-w-id="b3fd95f7-10b2-f103-e2b0-e28f97e2adb1 flex justify-between"
+            className="df-item  p-6 py-8 md:flex justify-around background_box"
+          >
+            <div className="md:w-1/2 mt-4">
+              <img
+                src="/top_image.svg"
+                 loading="lazy"
+                alt=""
+                className="icon-medium"
+              />
+              <h3 className="text-2xl mt-4">Doller House 1 Plan</h3>
+              <p className="text-white mt-3 max-w-[350px]">
+                The best & advanced networking platform deployed on Binance
+                smart chain network, with superfast transactions & speed. The
+                house of investment is coming for you all..
+              </p>
+            </div>
+            <div className="max-w-[425px] mt-4 md:mt-0 mx-auto md:mx-0">
+              <img src="/featuresBox3.svg" />
             </div>
           </div>
         </div>
