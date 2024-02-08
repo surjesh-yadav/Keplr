74843d29f55decf3d39c2553bbca4133f29b6d0a
SurjeshYadavMasai <surjeshom@gmail.com>
2024-02-08 20:54:36 +0530

    privacy policy and terms_conditions added

---
+++ app/privacy_policy/page.jsx
@@ -0,0 +1,108 @@
+import React from "react";
+// import "./style.css";
+// import group4_bg4 from "../image/background/svg-image-44.svg";
+
+const Privacy_policy = () => {
+  return (
+    <div>
+      <div className="disclaimer">
+        {/* <img className="group4_bg4" src={group4_bg4} alt="group4_bg4" /> */}
+
+        <div className="container max-w-[1100px] mx-auto px-5">
+          <div className="disclaimer_conatnt2">
+            <h2>Privacy Policy</h2>
+            <p className="disclaimer_conatnt2_p">
+              Thank you for using CUN.!!! This Privacy Policy outlines how we
+              collect, use, disclose, and safeguard your personal information.
+              By using cunetwork.ai, you agree to the terms outlined in this
+              policy.
+            </p>
+
+            <h3>1.Information We Collect</h3>
+            
+            <p>
+              a. <b>Personal Information:</b> We do not collect personal
+              information such as your name, email address, and wallet address
+              when you use CUN
+            </p>
+
+            <p>
+              b. <b>Transaction Data:</b> We do not collect information related
+              to your transactions, including transaction history and details
+              but stored on the blockchain with transaction hash.
+            </p>
+
+            <p>
+              c. <b>Device Information:</b> We do not collect information about
+              the device you use to access cunetwork.ai, including device type,
+              operating system, and IP address.
+            </p>
+
+            <h3>2.Do We Use Your Information:</h3>
+            <p>
+              a. <b>Transaction Processing:</b>
+              We do not use your personal information to process transactions
+              and provide you with the services offered by CUN
+            </p>
+            <p>
+              b. <b> Improving Services: </b>
+              We analyze data to enhance and improve the functionality and
+              performance of [Your Crypto Token].
+            </p>
+            <p>
+              c. <b>Communications:</b> We may use your email address to send
+              important updates, announcements, and information related to [Your
+              Crypto Token].
+            </p>
+
+            <h3>3.Information Sharing</h3>
+            <p>
+              {" "}
+              <b>a. Third Parties:</b>We may share your information with trusted
+              third parties for the purpose of providing and improving our
+              services. These third parties are obligated to maintain the
+              confidentiality and security of your information.
+            </p>
+            <p>
+              {" "}
+              <b>b. Legal Compliance:</b>We may disclose your information if
+              required by law or in response to legal requests.
+            </p>
+            <h3>4.Security</h3>
+            <p>
+              We implement reasonable security measures to protect your personal
+              information from unauthorized access, disclosure, alteration, and
+              destruction.
+            </p>
+
+            <h3>5. Your Choices</h3>
+            <p>
+              <b>a. Opt-Out:</b>You can opt-out of receiving non-essential
+              communications from us by following the instructions in the
+              communication.
+            </p>
+
+            <p>
+              <b>b. Access and Update:</b>You can access and update your
+              personal information by contacting us at on community channel
+            </p>
+
+            <h3>6. Changes to this Privacy Policy</h3>
+            <p>
+              We may update this Privacy Policy to reflect changes in our
+              practices. Please review this policy periodically for any updates.
+            </p>
+
+            <h3>7. Contact Us</h3>
+            <p>
+              If you have any questions or concerns about this Privacy Policy,
+              let us know..
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Privacy_policy;
---
+++ app/terms_conditions/page.jsx
@@ -0,0 +1,70 @@
+import React from 'react'
+ 
+
+const Terms_conditions = () => {
+  return (
+    <div>
+      <div className='disclaimer terms_conditions mx-auto max-w-[1100px]'>
+        {/* <img className="group4_bg4" src={group4_bg4} alt='group4_bg4' /> */}
+        <div className='container mx-auto'>
+          <div className='terms_condition'>
+          <h1>Terms conditions</h1>
+
+            <h3>1.Introduction</h3>
+            <p>Define the parties involved (users and the company/organization).
+              Briefly explain the purpose and nature of the cryptocurrency.</p>
+
+            <h3>2.Acceptance of Terms</h3>
+            <p>Clearly state that by using or accessing the cryptocurrency, users agree to abide by these terms and conditions.</p>
+
+            <h3>3.Registration and User Accounts</h3>
+            <p>Outline the process for user registration.
+              Highlight the responsibility of users to maintain the confidentiality of their accounts.</p>
+
+            <h3>4.Use of the Cryptocurrency</h3>
+            <p>Specify acceptable and prohibited uses.
+              Outline any restrictions related to age, jurisdiction, or other legal considerations.</p>
+
+            <h3>5.Transactions</h3>
+            <p>Detail the process for cryptocurrency transactions.
+              Clarify any fees associated with transactions.</p>
+
+            <h3>6.Privacy Policy</h3>
+            <p>Explain how user data is collected, used, and protected.
+              Include information on cookies, tracking, and third-party services</p>
+
+            <h3>7.Security</h3>
+            <p>Outline security measures implemented by the platform.
+              Advise users on security best practices and their responsibilities.</p>
+
+            <h3>8.Intellectual Property</h3>
+            <p>Specify ownership of intellectual property related to the cryptocurrency.
+              Include guidelines on the use of trademarks, logos, and other branding.</p>
+
+            <h3>9.Disclaimers</h3>
+            <p>Clarify that the cryptocurrency is provided "as is" and without warranties.
+              Disclaim any liability for losses or damages incurred by users.</p>
+
+            <h3>10.Termination</h3>
+            <p>Outline conditions under which the company can terminate a user's account.
+              Explain the process for dispute resolution.</p>
+
+            <h3>11.Governing Law</h3>
+            <p>Specify the jurisdiction and laws governing the terms and conditions.</p>
+
+            <h3>12.Amendments</h3>
+            <p>Reserve the right to amend the terms and conditions and communicate how users will be informed of changes.</p>
+
+            <h3>13.Contact Information</h3>
+            <p>Provide contact details for users to reach out for support or inquiries.</p>
+
+            <h3>14.Miscellaneous</h3>
+            <p>Include any additional clauses or information relevant to your cryptocurrency.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Terms_conditions
